squell/nts

commit 029c617e56bc56c1cc1a938307691783cb9f79f1

remove reliance on conditional compilation

.github/workflows/ci.yaml
@@ -18,10 +18,10 @@ jobs:
       - uses: actions/checkout@11bd71901bbe5b1630ceea73d27597364c9af683
 
       - name: build
-        run: CC="cc -Werror" make -C src SIV=OPENSSL demo qdntp
+        run: CC="cc -Werror" make -C src CRYPTO=openssl demo qdntp
 
       - name: run unit tests
-        run: CC="cc -fsanitize=address,undefined -Werror" make -C src SIV=OPENSSL test
+        run: CC="cc -fsanitize=address,undefined -Werror" make -C src CRYPTO=openssl test
 
   build-and-test-libaes_siv:
     runs-on: ubuntu-latest
@@ -37,10 +37,10 @@ jobs:
         run: make -C libaes_siv
 
       - name: build
-        run: CC="cc -fsanitize=address,undefined -Werror" make -C src SIV=LIBAES_SIV demo qdntp
+        run: CC="cc -fsanitize=address,undefined -Werror" make -C src CRYPTO=libaes_siv demo qdntp
 
       - name: run unit tests
-        run: CC="cc -fsanitize=address,undefined -Werror" make -C src SIV=LIBAES_SIV test
+        run: CC="cc -fsanitize=address,undefined -Werror" make -C src CRYPTO=libaes_siv test
 
   build-and-test-gcrypt:
     runs-on: ubuntu-latest
@@ -51,10 +51,10 @@ jobs:
         run: sudo apt-get install libgcrypt20-dev
 
       - name: build
-        run: CC="cc -Werror" make -C src SIV=GCRYPT demo qdntp
+        run: CC="cc -Werror" make -C src CRYPTO=gcrypt demo qdntp
 
       - name: run unit tests
-        run: CC="cc -fsanitize=address,undefined -Werror" make -C src SIV=GCRYPT test
+        run: CC="cc -fsanitize=address,undefined -Werror" make -C src CRYPTO=gcrypt test
 
   static-analysis-openssl:
     runs-on: ubuntu-latest
@@ -67,7 +67,7 @@ jobs:
           sudo apt-get install --yes clang-tools
 
       - name: run analysis
-        run: scan-build --status-bugs make -C src SIV=OPENSSL
+        run: scan-build --status-bugs make -C src CRYPTO=openssl
 
   static-analysis-libaes_siv:
     runs-on: ubuntu-latest
@@ -88,7 +88,7 @@ jobs:
         run: make -C libaes_siv
 
       - name: run analysis
-        run: scan-build --status-bugs make -C src SIV=LIBAES_SIV
+        run: scan-build --status-bugs make -C src CRYPTO=libaes_siv
 
   static-analysis-gcrypt:
     runs-on: ubuntu-latest
@@ -104,7 +104,7 @@ jobs:
         run: sudo apt-get install libgcrypt20-dev
 
       - name: run analysis
-        run: scan-build --status-bugs make -C src SIV=GCRYPT
+        run: scan-build --status-bugs make -C src CRYPTO=gcrypt
 
   smoke-test-ntp:
     runs-on: ubuntu-latest

src/nts_extfields.c
@@ -9,6 +9,11 @@
 #include "nts_extfields.h"
 #include "nts_crypto.h"
 
+#if OPENSSL_VERSION_PREREQ(3,5)
+#else
+#  define OPENSSL_WORKAROUND
+#endif
+
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 
 typedef struct {

src/Makefile
@@ -1,38 +1,29 @@
 .PHONY: all clean test fuzz fuzz_ntp fuzz_ntske
 
-SIV ?= LIBAES_SIV
-
 CFLAGS ?= -Wall -Wextra -pedantic -std=gnu11
-ifeq (${SIV},LIBAES_SIV)
-  CFLAGS += -I../libaes_siv -DUSE_LIBAES_SIV
+CRYPTO ?= openssl
+
+ifeq ($(CRYPTO),libaes_siv)
   LINK += ../libaes_siv/libaes_siv.a
-  CRYPTO = crypto_libaes_siv
+else ifeq ($(CRYPTO), gcrypt)
+  LINK += -lgcrypt
+else ifeq ($(CRYPTO), openssl)
+  # we already link to OpenSSL
 else
-  ifeq (${SIV}, GCRYPT)
-    CFLAGS += -DUSE_GCRYPT
-    LINK += -lgcrypt
-    CRYPTO = crypto_gcrypt
-  else
-    ifeq (${SIV}, OPENSSL)
-      CFLAGS += -DOPENSSL_WORKAROUND
-      CRYPTO = crypto
-    else
-      $(error "SIV must be either OPENSSL, GCRYPT, or LIBAES_SIV")
-    endif
-  endif
+  $(error "CRYPTO must be either 'openssl', 'gcrypt', or 'libaes_siv'")
 endif
 
-demo: demo.o nts_packet.o nts_ssl.o sntp.o nts_extfields.o nts_${CRYPTO}.o tcp_connect.o
-	$(CC) $+ ${LINK} -lssl -lcrypto -o $@
+demo: demo.o nts_packet.o nts_ssl.o sntp.o nts_extfields.o nts_crypto_$(CRYPTO).o tcp_connect.o
+	$(CC) $+ $(LINK) -lssl -lcrypto -o $@
 
 qdntp: qdntp.o sntp.o nts_extfields.o tcp_connect.o
 	$(CC) $+ -o $@
 
-nts_test: nts_packet.o nts_extfields.o nts_test.o nts_${CRYPTO}.o
-	$(CC) $+ ${LINK} -lssl -lcrypto -o $@
+nts_test: nts_packet.o nts_extfields.o nts_test.o nts_crypto_$(CRYPTO).o
+	$(CC) $+ $(LINK) -lssl -lcrypto -o $@
 
 nts_fuzz: nts_packet.o nts_extfields.o nts_fuzz.o
-	$(CC) $+ ${CFLAGS} -o $@
+	$(CC) $+ $(CFLAGS) -o $@
 
 test: nts_test
 	@./nts_test
@@ -40,20 +31,24 @@ test: nts_test
 
 FUZZTIME ?= 3600
 fuzz_ntske: nts_fuzz
-	afl-fuzz -V ${FUZZTIME} -i ../fuzz/nts_ke -o out_ntske -G 1280 -D ./nts_fuzz @@ +
+	afl-fuzz -V $(FUZZTIME) -i ../fuzz/nts_ke -o out_ntske -G 1280 -D ./nts_fuzz @@ +
 	@! test -e ./out_ntske/default/crashes/README.txt
 
 fuzz_ntp: nts_fuzz
-	afl-fuzz -V ${FUZZTIME} -i ../fuzz/nts_extfield -o out_ntp -g 48 -G 1280 -D ./nts_fuzz @@
+	afl-fuzz -V $(FUZZTIME) -i ../fuzz/nts_extfield -o out_ntp -g 48 -G 1280 -D ./nts_fuzz @@
 	@! test -e ./out_ntp/default/crashes/README.txt
 
-all: demo qdntp nts_fuzz nts_test
+all: demo qdntp nts_fuzz nts_test nts_crypto.o nts_crypto_libaes_siv.o nts_crypto_gcrypt.o
 
 clean:
 	rm -f a.out *.o demo qdntp nts_test nts_fuzz *.gcov *.gcno *.gcda
 
 demo.o: demo.c nts.h nts_extfields.h sntp.h
-nts_${CRYPTO}.o: nts_${CRYPTO}.c nts_crypto.h nts.h nts_extfields.h
+nts_crypto_openssl.o: nts_crypto_openssl.c nts_crypto.h nts.h nts_extfields.h
+nts_crypto_gcrypt.o: nts_crypto_gcrypt.c nts_crypto.h nts.h nts_extfields.h
+nts_crypto_libaes_siv.o: nts_crypto_libaes_siv.c nts_crypto.h nts.h nts_extfields.h
+	$(CC) $(CFLAGS) -I../libaes_siv -o $@ -c $<
+
 nts_extfields.o: nts_extfields.c nts_extfields.h nts.h nts_crypto.h
 nts_fuzz.o: nts_fuzz.c nts.h nts_extfields.h
 nts_packet.o: nts_packet.c nts.h

src/nts_crypto_gcrypt.c
@@ -3,6 +3,26 @@
 #include <assert.h>
 #include <gcrypt.h>
 
+static const struct NTS_AEAD_param supported_algos[] = {
+	{ NTS_AEAD_AES_SIV_CMAC_256, 256/8, 16, 16, true, false, "AES-128-SIV" },
+	{ NTS_AEAD_AES_SIV_CMAC_512, 512/8, 16, 16, true, false, "AES-256-SIV" },
+	{ NTS_AEAD_AES_SIV_CMAC_384, 384/8, 16, 16, true, false, "AES-192-SIV" },
+	{ NTS_AEAD_AES_128_GCM_SIV,  128/8, 16, 12, false, true, "AES-128-GCM-SIV" },
+	{ NTS_AEAD_AES_256_GCM_SIV,  256/8, 16, 12, false, true, "AES-256-GCM-SIV" },
+};
+
+#define ELEMS(array) (sizeof(array) / sizeof(*array))
+
+const struct NTS_AEAD_param *NTS_AEAD_param(NTS_AEAD_algorithm_type id) {
+	for(size_t i=0; i < ELEMS(supported_algos); i++) {
+		if(supported_algos[i].aead_id == id) {
+			return &supported_algos[i];
+		}
+	}
+
+	return NULL;
+}
+
 #define check(expr) if(expr); else goto exit;
 
 static int process_assoc_data(

src/nts_crypto_libaes_siv.c
@@ -3,6 +3,24 @@
 #include <assert.h>
 #include <aes_siv.h>
 
+static const struct NTS_AEAD_param
+	siv256 = { NTS_AEAD_AES_SIV_CMAC_256, 256/8, 16, 16, true, false, "AES-128-SIV" },
+	siv512 = { NTS_AEAD_AES_SIV_CMAC_512, 512/8, 16, 16, true, false, "AES-256-SIV" },
+	siv384 = { NTS_AEAD_AES_SIV_CMAC_384, 384/8, 16, 16, true, false, "AES-192-SIV" };
+
+const struct NTS_AEAD_param *NTS_AEAD_param(NTS_AEAD_algorithm_type id) {
+	switch(id) {
+		case NTS_AEAD_AES_SIV_CMAC_256:
+			return &siv256;
+		case NTS_AEAD_AES_SIV_CMAC_512:
+			return &siv512;
+		case NTS_AEAD_AES_SIV_CMAC_384:
+			return &siv384;
+		default:
+			return NULL;
+	}
+}
+
 #define check(expr) if(expr); else goto exit;
 
 /* caller should make sure that there is enough room in ptxt for holding the plaintext + one additional block */

src/nts_crypto_openssl.c
@@ -3,6 +3,28 @@
 #include <assert.h>
 #include <openssl/ssl.h>
 
+static const struct NTS_AEAD_param supported_algos[] = {
+	{ NTS_AEAD_AES_SIV_CMAC_256, 256/8, 16, 16, true, false, "AES-128-SIV" },
+	{ NTS_AEAD_AES_SIV_CMAC_512, 512/8, 16, 16, true, false, "AES-256-SIV" },
+	{ NTS_AEAD_AES_SIV_CMAC_384, 384/8, 16, 16, true, false, "AES-192-SIV" },
+#if OPENSSL_VERSION_PREREQ(3,2)
+	{ NTS_AEAD_AES_128_GCM_SIV,  128/8, 16, 12, false, true, "AES-128-GCM-SIV" },
+	{ NTS_AEAD_AES_256_GCM_SIV,  256/8, 16, 12, false, true, "AES-256-GCM-SIV" },
+#endif
+};
+
+#define ELEMS(array) (sizeof(array) / sizeof(*array))
+
+const struct NTS_AEAD_param *NTS_AEAD_param(NTS_AEAD_algorithm_type id) {
+	for(size_t i=0; i < ELEMS(supported_algos); i++) {
+		if(supported_algos[i].aead_id == id) {
+			return &supported_algos[i];
+		}
+	}
+
+	return NULL;
+}
+
 #define check(expr) if(expr); else goto exit;
 
 static int process_assoc_data(

src/nts_fuzz.c
@@ -62,3 +62,8 @@ int NTS_decrypt(unsigned char *ptxt, const unsigned char *ctxt, int ctxt_len, co
 	memmove(ptxt, ctxt+16, ctxt_len - BLKSIZ);
 	return ctxt_len - BLKSIZ;
 }
+
+const struct NTS_AEAD_param *NTS_AEAD_param(NTS_AEAD_algorithm_type id) {
+	static struct NTS_AEAD_param param = { NTS_AEAD_AES_SIV_CMAC_256, 256/8, 16, 16, true, false, "AES-128-SIV" };
+	return id? &param : NULL;
+}

src/nts_packet.c
@@ -258,23 +258,3 @@ int NTS_decode_response(unsigned char *buffer, size_t buf_size, struct NTS_agree
 	return -1;
 }
 #undef check
-
-static const struct NTS_AEAD_param supported_algos[] = {
-	{ NTS_AEAD_AES_SIV_CMAC_256, 256/8, 16, 16, true, false, "AES-128-SIV" },
-	{ NTS_AEAD_AES_SIV_CMAC_512, 512/8, 16, 16, true, false, "AES-256-SIV" },
-	{ NTS_AEAD_AES_SIV_CMAC_384, 384/8, 16, 16, true, false, "AES-192-SIV" },
-#ifndef USE_LIBAES_SIV
-	{ NTS_AEAD_AES_128_GCM_SIV,  128/8, 16, 12, false, true, "AES-128-GCM-SIV" },
-	{ NTS_AEAD_AES_256_GCM_SIV,  256/8, 16, 12, false, true, "AES-256-GCM-SIV" },
-#endif
-};
-
-const struct NTS_AEAD_param *NTS_AEAD_param(NTS_AEAD_algorithm_type id) {
-	for(size_t i=0; i < ELEMS(supported_algos); i++) {
-		if(supported_algos[i].aead_id == id) {
-			return &supported_algos[i];
-		}
-	}
-
-	return NULL;
-}

commit 89adefead99ceaa89582a9da110100818c3c32c1

fix openssl workaround breaking even in gcrypt/libaes mode
introduced in 029c617e56bc56c1cc1a938307691783cb9f79f1

.github/workflows/ci.yaml
@@ -34,14 +34,16 @@ jobs:
       - name: build with clang
         run: CC="clang -fsanitize=address,integer,undefined -Werror" make -B -C src CRYPTO=openssl all
 
-      - name: test with openssl
-        run: CC="clang -fsanitize=address,integer,undefined -Werror" make -C src CRYPTO=openssl test
-
       - name: test with gcrypt
         run: CC="clang -fsanitize=address,integer,undefined -Werror" make -C src CRYPTO=gcrypt test
 
       - name: test with libaes_siv
         run: CC="clang -fsanitize=address,integer,undefined -Werror" make -C src CRYPTO=libaes_siv test
+
+      - name: test with openssl
+        run: |
+          CC="clang -fsanitize=address,integer,undefined -Werror -DOPENSSL_WORKAROUND" make -C src -B nts_extfields.o
+          CC="clang -fsanitize=address,integer,undefined -Werror" make -C src CRYPTO=openssl test
 
       - name: install clang-tools
         run: |

src/nts_extfields.c
@@ -11,9 +11,8 @@
 #include "nts_extfields.h"
 #include "nts_crypto.h"
 
-#if OPENSSL_VERSION_PREREQ(3,5)
-#else
-#  define OPENSSL_WORKAROUND
+#if defined(OPENSSL_WORKAROUND) && OPENSSL_VERSION_PREREQ(3,5)
+#  warning The OpenSSL workaround is not necessary.
 #endif
 
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"